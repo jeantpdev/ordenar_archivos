3b591c0749b33d5cd995fb40c5f41cb56df2b8ea
Jean Trujillo <91339890+zacksPerez43@users.noreply.github.com>
2023-03-29 22:39:16 -0500

    Bug de extensiones arreglado
    Bug extensiones: Al ejecutar el script, se movian los archivos pero quedaban sin extension por lo que quedaban innutilizables (Al intentar asignarle una extensión, no se aplicaba el cambio).

--- ordena_carpetas.py
+++ ordena_carpetas.py
@@ -1,61 +1,85 @@
 import os
-import shutil
 import errno
+import shutil
 from getpass import getuser
 
 ext_texto = ['.docx', '.txt', '.doc','.pdf', '.pptx']
 ext_imagenes = ['.png', '.jpg', '.jpeg', '.gif']
-ext_video = ['.mov', '.mp4']
-
+ext_video = ['.mov', '.mp4', '.mkv']
+ext_archivos = ['.rar','.zip']
 usuario = getuser()
 
-#if os.path.isdir(f'C:\\Users\\{usuario}\\Downloads\\'):
-#    print('La carpeta existe.');
-#    ruta_descargas = f'C:\\Users\\{usuario}\\Downloads\\'
-#    print(ruta_descargas)
-#else:
-#    ruta_descargas = f'C:\\Usuarios\\{usuario}\\Descargas\\'
-#    print('No existe')
+def crear_carpetas(ruta_descargas):
+
+    try:
+        if ruta_descargas+'Texto' == True:
+            print("Carpeta texto existe")
+        else:
+            os.mkdir(ruta_descargas+'Texto')
+            print("Carpeta texto creada")
+
+        if ruta_descargas+'Videos' == True:
+            print("Carpeta videos Existe")
+        else:
+            os.mkdir(ruta_descargas+'Videos')
+            print("Carpeta videos creada")
+
+        if ruta_descargas+'Imagenes' == True:
+            print("Carpeta imagenes Existe")
+        else:
+            os.mkdir(ruta_descargas+'Imagenes')
+            print("Carpeta imagenes creada")
+
+        if ruta_descargas+'Otros' == True:
+            print("Carpeta otros Existe")
+        else:
+            os.mkdir(ruta_descargas+'Otros')
+            print("Carpeta otros creada")
+
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+def nosexd():
+    for archivo in os.listdir(ruta_descargas):       
+        nombre_archivo, ext = os.path.splitext(archivo)
+        ordenar(nombre_archivo, ext, ruta_descargas)
+
+def existe_ruta():
+
+    usuario = getuser()
+
+    #Se comprueba que exista la carpeta descargas para el idioma inglés
+    if os.path.isdir(f'C:\\Users\\{usuario}\\Downloads\\'):
+        ruta_descargas = f'C:\\Users\\{usuario}\\Downloads\\'
+        print(ruta_descargas)
+        return ruta_descargas
+    else:
+        ruta_descargas = f'C:\\Usuarios\\{usuario}\\Descargas\\'
+        print(ruta_descargas)
+        return ruta_descargas
 
 def ordenar (archivo, ext, ruta_descargas):
     for i in ext_texto:
         if ext == i:
-            shutil.move(ruta_descargas + archivo, ruta_descargas + 'Texto')
+            shutil.move(ruta_descargas + archivo+ext, ruta_descargas + 'Texto')
 
     for i in ext_video:
         if ext == i:
-            shutil.move(ruta_descargas + archivo, ruta_descargas + 'Videos')      
+            shutil.move(ruta_descargas + archivo+ext, ruta_descargas + 'Videos')      
 
     for i in ext_imagenes:
         if ext == i:
-            shutil.move(ruta_descargas + archivo, ruta_descargas + 'Imagenes')
+            shutil.move(ruta_descargas + archivo+ext, ruta_descargas + 'Imagenes')
 
-def crear_carpetas():
-    try:
-        os.mkdir(ruta_descargas+'Texto')
-        os.mkdir(ruta_descargas+'Videos')
-        os.mkdir(ruta_descargas+'Imagenes')
-        os.mkdir(ruta_descargas+'Otros')
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
-#def main():
-    #crear_carpetas()
-    #for archivo in os.listdir(ruta_descargas):       
-        #nombre_archivo, ext = os.path.splitext(archivo)
-        #ordenar(archivo, ext, ruta_descargas)
+    for i in ext_archivos:
+        if ext == i:
+            shutil.move(ruta_descargas + archivo+ext, ruta_descargas + 'Otros')
 
 if __name__ == '__main__':
 
-    usuario = getuser()
-
-    if os.path.isdir(f'C:\\Users\\{usuario}\\Downloads\\'):
-        print('La carpeta existe.')
-        ruta_descargas = f'C:\\Users\\{usuario}\\Downloads\\'
-        print(ruta_descargas)
-    else:
-        ruta_descargas = f'C:\\Usuarios\\{usuario}\\Descargas\\'
-        print('No existe')
-
+    ruta_descargas = existe_ruta()
+    crear_carpetas(ruta_descargas)
+    nosexd()
 
 
